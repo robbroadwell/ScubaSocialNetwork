9679f185e917435551ae6d08ddb76069618596a1
Rob Broadwell <rob@inspiringapps.com>
2020-05-27 23:07:32 -0400

    logout

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, TextInput, Image } from 'react-native';
 import { connect } from "react-redux";
 import { setUser } from "../redux/actions";
+import { getUser } from "../redux/selectors";
 const axios = require('axios')
 
 class Login extends Component {
@@ -22,18 +23,22 @@ class Login extends Component {
     };
 
     onPressSubmit = () => {
-        const { setUser, disableLoginMode } = this.props;
-
         if (this.state.username !== "" && this.state.password !== "") {
             axios.post('https://www.divingscore.com/api/users/login', {
                 username: this.state.username,
                 password: this.state.password
               })
               .then(function (response) {
-                    setUser(response.data.user);
-                    disableLoginMode();
-              })
+                    this.setState({ username: "", password: "" });
+                    this.props.setUser(response.data.user);
+                    this.props.disableLoginMode();
+              }.bind(this));
         }
+    }
+
+    onPressLogout = () => {
+        this.props.setUser([]);
+        this.props.disableLoginMode();
     }
     
     render() {
@@ -47,26 +52,48 @@ class Login extends Component {
                     <TouchableOpacity style={{position: 'absolute', top: 5, right: 5}} onPress={this.props.disableLoginMode}>
                         <Image style={{height: 25, width: 25, margin: 5, tintColor: 'black'}} source={require('../assets/close.png')} />
                     </TouchableOpacity>
-                    <TextInput
-                        style={{ height: 40, borderColor: 'gray', borderWidth: 1, padding: 10, marginBottom: 10 }}
-                        onChangeText={text => this.onChangeTextUsername(text)}
-                        value={this.state.username}
+
+                    {this.props.user.username ? 
+                    
+                    <View>
+                        <TouchableOpacity onPress={() => this.onPressLogout()}>
+                            <Text style={{textAlign: 'center', margin: 20, color: 'black', fontWeight: 'bold', fontSize: 18}}>Logout</Text>
+                        </TouchableOpacity>
+                    </View> 
+
+                    : 
+
+                    <View>
+                        <TextInput
+                            style={{ height: 40, borderColor: 'gray', borderWidth: 1, padding: 10, marginBottom: 10 }}
+                            onChangeText={text => this.onChangeTextUsername(text)}
+                            placeholder={'Username'}
+                            value={this.state.username}
+                            />
+                        <TextInput
+                            style={{ height: 40, borderColor: 'gray', padding: 10, borderWidth: 1 }}
+                            onChangeText={text => this.onChangeTextPassword(text)}
+                            placeholder={'Password'}
+                            value={this.state.password}
                         />
-                    <TextInput
-                        style={{ height: 40, borderColor: 'gray', padding: 10, borderWidth: 1 }}
-                        onChangeText={text => this.onChangeTextPassword(text)}
-                        value={this.state.password}
-                    />
-                    <TouchableOpacity onPress={() => this.onPressSubmit()}>
-                        <Text style={{textAlign: 'center', margin: 20, color: 'black', fontWeight: 'bold', fontSize: 18}}>Login</Text>
-                    </TouchableOpacity>
+                        <TouchableOpacity onPress={() => this.onPressSubmit()}>
+                            <Text style={{textAlign: 'center', margin: 20, color: 'black', fontWeight: 'bold', fontSize: 18}}>Login</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    }
                 </View>
             </View>
         )
     }
 }
 
-export default connect(
-    null,
+const mapStateToProps = state => {
+    const user = getUser(state);
+    return { user };
+  };
+  
+  export default connect(
+    mapStateToProps,
     { setUser }
   )(Login);
