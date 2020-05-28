92733a2925d98a3bcc361131f0650f8e754bb4de
Rob Broadwell <rob@inspiringapps.com>
2020-05-27 22:34:50 -0400

    redux-persist to maintain redux store after reload

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import Root from './components/Root'
 import ReactGA from 'react-ga';
-import store from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
+import configureStore from './redux/store';
+const { persistor, store } = configureStore()
+
 
 ReactGA.initialize('UA-88100612-2');
 
 function App() {
   return (
     <Provider store={store}>
-      <Root />
+       <PersistGate loading={null} persistor={persistor}>
+        <Root />
+       </PersistGate>
     </Provider>
   );
 }
