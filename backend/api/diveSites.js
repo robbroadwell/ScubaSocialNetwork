6e3dafa59a055cd707a6427b9b24c0ea46a94d8c
Rob Broadwell <rob@inspiringapps.com>
2020-05-29 00:42:12 -0400

    bit more formatting

--- backend/api/diveSites.js
+++ backend/api/diveSites.js
@@ -5,70 +5,68 @@ const passport = require('passport');
 const DiveSite = require('../models/diveSite');
 
 router.get('/', (req, res) => {
-    const polygon = req.query.polygon.split(',')
-    
-    DiveSite.find({
-        location: { 
-            $geoWithin: {  
-                $geometry: {
-                    type: "Polygon",
-                    coordinates: [[
-                        [ parseFloat(polygon[0]), parseFloat(polygon[1]) ],
-                        [ parseFloat(polygon[2]), parseFloat(polygon[3]) ],
-                        [ parseFloat(polygon[4]), parseFloat(polygon[5]) ],
-                        [ parseFloat(polygon[6]), parseFloat(polygon[7]) ],
-                        [ parseFloat(polygon[8]), parseFloat(polygon[9]) ],
-                    ]]
-                  }
-        }}
-    })
-    .then(diveSites => res.json(diveSites))
-    .catch(err => console.log(err))
+  const polygon = req.query.polygon.split(',')
+
+  DiveSite.find({
+    location: {
+      $geoWithin: {
+        $geometry: {
+          type: "Polygon",
+          coordinates: [[
+            [ parseFloat(polygon[0]), parseFloat(polygon[1]) ],
+            [ parseFloat(polygon[2]), parseFloat(polygon[3]) ],
+            [ parseFloat(polygon[4]), parseFloat(polygon[5]) ],
+            [ parseFloat(polygon[6]), parseFloat(polygon[7]) ],
+            [ parseFloat(polygon[8]), parseFloat(polygon[9]) ],
+          ]]
+        }
+    }}
+  })
+  .then(diveSites => res.json(diveSites))
+  .catch(err => console.log(err))
 })
 
 router.post('/', (req, res, next) => {
-    passport.authenticate('jwt', { session: false }, (err, user, info) => {
-        if (err) {
-          console.error(err);
-        }
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      console.error(err);
+    }
 
-        console.log(user);
-
-        if (info !== undefined) {
-          console.error(info.message);
-          res.status(403).send(info.message);
-        } else if (!user) {
-          console.error('user authorizing the JWT not found');
-          res.status(403).send('user authorizing the JWT not found');
-        } else {
-            const { name, country, latitude, longitude } = req.body;
-            const newDiveSite = new DiveSite({
-                name: name, 
-                country: country,
-                location: {
-                    type: "Point",
-                    coordinates: [
-                        longitude, latitude
-                    ]
-                },
-                reviews: []
-                // depth: depth,
-                // visibility: visibility,
-                // access: access,
-                // description: description,
-                // rating: rating
-        })
-        newDiveSite.save()
-            .then(() => res.json({
-                message: "Created dive site successfully"
-            }))
-            .catch(err => res.status(400).json({
-                "error": err,
-                "message": "Error creating dive site"
-            }))
-        }
-    })(req, res, next);
-  });
+    if (info !== undefined) {
+      console.error(info.message);
+      res.status(403).send(info.message);
+    } else if (!user) {
+      console.error('user authorizing the JWT not found');
+      res.status(403).send('user authorizing the JWT not found');
+    } else {
+        const { name, country, latitude, longitude } = req.body;
+        const newDiveSite = new DiveSite({
+          name: name,
+          country: country,
+          location: {
+            type: "Point",
+            coordinates: [
+                longitude, latitude
+            ]
+          },
+          reviews: []
+          // depth: depth,
+          // visibility: visibility,
+          // access: access,
+          // description: description,
+          // rating: rating
+    })
+    newDiveSite.save()
+      .then(() => res.json({
+        message: "Created dive site successfully"
+      }))
+      .catch(err => res.status(400).json({
+        "error": err,
+        "message": "Error creating dive site"
+      }))
+    }
+  })(req, res, next);
+});
 
 
-module.exports = router 
+module.exports = router
--- backend/api/passport.js
+++ backend/api/passport.js
@@ -7,86 +7,86 @@ const jwtSecret = require('./jwtSecret');
 const bcrypt = require('bcrypt');
 
 passport.use(
-    'login',
-    new LocalStrategy(
-      {
-        usernameField: 'username',
-        passwordField: 'password',
-        session: false,
-      },
-      (username, password, done) => {
-        try {
-            User.findOne({ username: username }, function (err, user) {
-
-            if (user === null) {
-              return done(null, false, { message: 'bad username' });
-            }
-
-            bcrypt.compare(password, user.password).then(response => {
-              if (response !== true) {
-                console.log('passwords do not match');
-                return done(null, false, { message: 'passwords do not match' });
-              }
-              console.log('user found & authenticated');
-              return done(null, user);
-            });
-          });
-        } catch (err) {
-          done(err);
-        }
-      },
-    ),
-  );
-
-  passport.use(
-    'register',
-    new LocalStrategy(
-      {
-        usernameField: 'username',
-        passwordField: 'password',
-        session: false,
-      },
-      (username, password, done) => {
-        console.log("progress");
-        try {
-            User.findOne({ username: username }, function (err, user) {
-
-                console.log(user);
-            if (user !== null) {
-              return done(null, false, { message: 'username already taken' });
-            }
-
-            return done(null, true);
-          });
-        } catch (err) {
-          done(err);
-        }
-      },
-    ),
-  );
-
-  const opts = {
-    jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
-    secretOrKey: jwtSecret.secret,
-  };
-  
-  passport.use(
-    'jwt',
-    new JWTstrategy(opts, (jwt_payload, done) => {
-      console.log("passport.js")
-      console.log(jwt_payload.id)
+  'login',
+  new LocalStrategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password',
+      session: false,
+    },
+    (username, password, done) => {
       try {
-        User.findById(jwt_payload.id).then(user => {
-          if (user) {
-            console.log('user found in db in passport');
-            done(null, user);
-          } else {
-            console.log('user not found in db');
-            done(null, false);
+        User.findOne({ username: username }, function (err, user) {
+
+          if (user === null) {
+            return done(null, false, { message: 'bad username' });
           }
+
+          bcrypt.compare(password, user.password).then(response => {
+            if (response !== true) {
+              console.log('passwords do not match');
+              return done(null, false, { message: 'passwords do not match' });
+            }
+            console.log('user found & authenticated');
+            return done(null, user);
+          });
         });
       } catch (err) {
         done(err);
       }
-    }),
-  );
+    },
+  ),
+);
+
+passport.use(
+  'register',
+  new LocalStrategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password',
+      session: false,
+    },
+    (username, password, done) => {
+      console.log("progress");
+      try {
+        User.findOne({ username: username }, function (err, user) {
+
+          console.log(user);
+          if (user !== null) {
+            return done(null, false, { message: 'username already taken' });
+          }
+
+          return done(null, true);
+        });
+      } catch (err) {
+        done(err);
+      }
+    },
+  ),
+);
+
+const opts = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
+  secretOrKey: jwtSecret.secret,
+};
+
+passport.use(
+  'jwt',
+  new JWTstrategy(opts, (jwt_payload, done) => {
+    console.log("passport.js")
+    console.log(jwt_payload.id)
+    try {
+      User.findById(jwt_payload.id).then(user => {
+        if (user) {
+          console.log('user found in db in passport');
+          done(null, user);
+        } else {
+          console.log('user not found in db');
+          done(null, false);
+        }
+      });
+    } catch (err) {
+      done(err);
+    }
+  }),
+);
