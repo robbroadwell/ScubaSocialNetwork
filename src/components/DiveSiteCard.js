139f01487eae190f3ff8eb6a5580070f9af5c520
Rob Broadwell <rob@inspiringapps.com>
2020-06-01 14:44:55 -0400

    don’t bold dive site names

--- src/components/DiveSiteCard.js
+++ src/components/DiveSiteCard.js
@@ -63,9 +63,9 @@ class DiveSiteCard extends Component {
               <View style={{flexDirection: 'row'}}>
 
                   <View>
-                    <Text style={{fontSize: 16, fontWeight: 'bold'}}>{this.props.site.name}</Text>
+                    <Text style={{fontSize: 16}}>{this.props.site.name}</Text>
                     <View style={{flexDirection: 'row', alignItems: 'flex-end', marginTop: 3}}>
-                      <Text style={{fontSize: 14, fontWeight: 'bold'}}>{this.props.site.country}, </Text>
+                      <Text style={{fontSize: 14}}>{this.props.site.country}, </Text>
                       <Text style={{fontSize: 13}}>{ Number((this.props.site.location.coordinates[1]).toFixed(5))}, {Number((this.props.site.location.coordinates[0]).toFixed(5))}</Text>
                     </View>
                   </View>
