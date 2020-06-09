2dd0a2b010331c0e96d672a71fde12e9baa7e36e
Rob Broadwell <rob@inspiringapps.com>
2020-06-09 09:31:35 -0400

    Alert color

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -8,7 +8,7 @@ class Alert extends Component {
 
   render() {
     return (
-      <View style={{backgroundColor: "#540B0E", flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 30, borderTopWidth: 1, borderColor: "#DDDDDD"}}>
+      <View style={{backgroundColor: "#21313C", flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 30, borderTopWidth: 1, borderColor: "#DDDDDD"}}>
         <Text style={{color: '#DDDDDD'}}>We are brand new! Launched on June 15, 2020. Please check back often for updates, and we welcome any </Text>
         <Text style={{textDecorationLine: 'underline', color: '#DDDDDD'}}>comments or suggestions!</Text>
         <TouchableOpacity style={{position: 'absolute', right: 0, height: '100%', paddingHorizontal: 10, justifyContent: 'center'}} >
