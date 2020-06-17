31fc034359f35e685175bfe2ba7d8d1cce571954
Rob Broadwell <rob@inspiringapps.com>
2020-06-16 23:25:51 -0400

    improved map and list layouts

--- src/components/destinations/DestinationDetail.js
+++ src/components/destinations/DestinationDetail.js
@@ -10,53 +10,54 @@ class DestinationDetail extends Component {
 
         <View style={{height: 500, margin: 20}}>
           <View style={{flexDirection: 'row'}}>
-            <Text style={{fontSize: 30, fontWeight: '700', color: 'black'}}>Mexico</Text>
-            <View style={{marginLeft: 15, justifyContent: 'center'}}>
-              <View style={{backgroundColor: '#A00000'}}>
-                <Text style={{padding: 5, color: 'white'}}>TOP</Text>
+
+            <View>
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{fontSize: 30, fontWeight: '700', color: 'black'}}>Mexico</Text>
+                <View style={{marginLeft: 15, justifyContent: 'center'}}>
+                  <View style={{backgroundColor: '#A00000'}}>
+                    <Text style={{padding: 5, color: 'white'}}>TOP</Text>
+                  </View>
+                </View>
+              </View>
+
+              <View style={{flexDirection: 'row', marginTop: 5, marginBottom: 10}}>
+                <Text style={{fontSize: 15}}>Destinations</Text>
+                <Image style={{width: 20, height: 20}} source={require('../../assets/right.svg')} />
+                <Text style={{fontSize: 15}}>Mexico</Text>
+                <Image style={{width: 20, height: 20}} source={require('../../assets/right.svg')} />
+                <Text style={{fontSize: 15}}>Select Region</Text>
+              </View>
+            </View>
+            
+            <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'flex-end'}}>
+
+              <View style={{flexDirection: 'row'}}>
+                <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
+                  <Text style={{padding: 10}}>Visibility</Text>
+                </View>
+                <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
+                  <Text style={{padding: 10}}>Depth</Text>
+                </View>
+                <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
+                  <Text style={{padding: 10}}>Water Temperature</Text>
+                </View>
+                <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
+                  <Text style={{padding: 10}}>Currents</Text>
+                </View>
+                <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
+                  <Text style={{padding: 10}}>Access</Text>
+                </View>
+                <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
+                  <Text style={{padding: 10}}>Animal Sightings</Text>
+                </View>
               </View>
             </View>
           </View>
 
-          <View style={{flexDirection: 'row', marginTop: 5, marginBottom: 10}}>
-            <Text style={{fontSize: 15}}>Destinations</Text>
-            <Image style={{width: 20, height: 20}} source={require('../../assets/right.svg')} />
-            <Text style={{fontSize: 15}}>Mexico</Text>
-            <Image style={{width: 20, height: 20}} source={require('../../assets/right.svg')} />
-            <Text style={{fontSize: 15}}>Select Region</Text>
-          </View>
 
-          <View style={{flexDirection: 'row', marginTop: 5, marginVertical: 10}}>
-            <View style={{width: 200, justifyContent: 'center', alignItems: 'center'}}>
-              {/* <Text style={{fontSize: 20, fontWeight: '700', color: 'black'}}>Dive Sites</Text>  */}
-            </View>
-            <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
-              <Text style={{padding: 10}}>Country</Text>
-            </View>
-            <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
-              <Text style={{padding: 10}}>Region</Text>
-            </View>
-            <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
-              <Text style={{padding: 10}}>Visibility</Text>
-            </View>
-            <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
-              <Text style={{padding: 10}}>Depth</Text>
-            </View>
-            <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
-              <Text style={{padding: 10}}>Water Temperature</Text>
-            </View>
-            <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
-              <Text style={{padding: 10}}>Currents</Text>
-            </View>
-            <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
-              <Text style={{padding: 10}}>Access</Text>
-            </View>
-            <View style={{borderColor: '#DDDDDD', borderWidth: 1, marginRight: 10}}>
-              <Text style={{padding: 10}}>Animal Sightings</Text>
-            </View>
-          </View>
 
-          <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{flex: 1, flexDirection: 'row', marginTop: 10}}>
             <List style={this.props.style} history={this.props.history} />
             {this.props.map}
           </View>
