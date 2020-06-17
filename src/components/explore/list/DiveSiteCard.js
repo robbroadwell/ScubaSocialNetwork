86b1d97acf626a0f2fece14eb3960be515ab6c79
Rob Broadwell <rob@inspiringapps.com>
2020-06-16 23:14:47 -0400

    old style dive site card

--- src/components/explore/list/DiveSiteCard.js
+++ src/components/explore/list/DiveSiteCard.js
@@ -13,6 +13,8 @@ class DiveSiteCard extends Component {
     var total = 0;
     var x;
 
+    console.log(this.props.site)
+
     for (x in this.props.site.reviews) {
       total = total + this.props.site.reviews[x].rating
     }
@@ -29,21 +31,22 @@ class DiveSiteCard extends Component {
 
   render() {
     return (
-      <View style={{borderLeftWidth: 1, borderLeftColor: '#CCCCCC'}}>
+      <View>
           <TouchableOpacity onPress={this.props.onPress} activeOpacity={1.0} >
             <BaseHoverableView
-              style={{ shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.0, shadowRadius: 5, shadowColor: '#000', marginBottom: 0, padding: this.props.selected ? 9 : 10, backgroundColor: this.props.selected ? "21313C" : '#FEFEFE', borderColor: this.props.selected ? '#555555' : '#cccccc', borderBottomWidth: this.props.selected ? 2 : 1 }}
-              onHover={{ shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.2, shadowRadius: 5, shadowColor: '#000', marginBottom: 0,  padding: this.props.selected ? 9 : 10, backgroundColor: '#FEFEFE', borderColor: this.props.selected ? '#555555' : '#aaaaaa', borderBottomWidth: this.props.selected ? 2 : 1 }}
+              style={{ shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.0, shadowRadius: 5, shadowColor: '#000', margin: 5, marginBottom: 0, padding: this.props.selected ? 9 : 10, backgroundColor: this.props.selected ? "21313C" : '#FEFEFE', borderColor: this.props.selected ? '#555555' : '#cccccc', borderWidth: this.props.selected ? 2 : 1 }}
+              onHover={{ shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.2, shadowRadius: 5, shadowColor: '#000', margin: 5, marginBottom: 0,  padding: this.props.selected ? 9 : 10, backgroundColor: '#FEFEFE', borderColor: this.props.selected ? '#555555' : '#aaaaaa', borderWidth: this.props.selected ? 2 : 1 }}
             >
 
-            <View style={{flexDirection: 'row', marginLeft: 10, marginRight: 20}}>
-              <View>
-                <Text style={{fontSize: 15, fontWeight: '500'}}>{this.props.site.name}</Text>
-                <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
-                <DiveSiteReviews reviews={this.reviews()} rating={this.rating()} />
+            <View style={{flexDirection: 'row'}}>
+                <View style={{marginRight: 20}}>
+                  <Text style={{fontSize: 16, fontWeight: '500'}}>{this.props.site.name}</Text>
+                  <View style={{flexDirection: 'row', alignItems: 'flex-end', marginTop: 2}}>
+                    <Text style={{fontSize: 13}}>{this.props.site.country} </Text>
+                    {/* <Text style={{fontSize: 13}}>{ Number((this.props.site.location.coordinates[1]).toFixed(4))}, {Number((this.props.site.location.coordinates[0]).toFixed(4))}</Text> */}
+                  </View>
                 </View>
-              </View>
-              
+                <DiveSiteReviews reviews={this.reviews()} rating={this.rating()} />
             </View>
           </BaseHoverableView>
         </TouchableOpacity>
