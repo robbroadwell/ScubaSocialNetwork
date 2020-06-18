42cd613c4daaa91364cf51905ed033bebcbd976a
Rob Broadwell <rob@inspiringapps.com>
2020-06-17 23:07:03 -0400

    attempt to fix cannot get

--- backend/server.js
+++ backend/server.js
@@ -40,8 +40,8 @@ app.use((req, res, next) => {
   });
 
 app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'))
 })
 
 const port = process.env.PORT || 8080;
