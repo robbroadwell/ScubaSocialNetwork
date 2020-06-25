3ab2e622989038d3c1d8b2e18ba778be04a7b8d4
Rob Broadwell <rob@inspiringapps.com>
2020-06-25 14:15:38 -0400

    disable verbose, ADD WORKS

--- src/components/dive-sites/DiveSiteAddView.js
+++ src/components/dive-sites/DiveSiteAddView.js
@@ -8,23 +8,26 @@ import { getUser, getMapCenter, getDestinations } from '../../redux/selectors';
 import BaseURL from '../../utility/BaseURL';
 const axios = require('axios')
 
-export const ADD_STEPS = {
-    LOCATION: "LOCATION",
-    NAME: "NAME"
-}
-
 class DiveSiteAddView extends Component {
 
   constructor(props) {
-      super(props);
-      this.state = {
-          name: "",
-          countryList: [],
-          countrySelected: null
-      };
-    }
+    super(props);
+    this.state = {
+        step: 1,
+        name: "",
+        countryList: [],
+        countrySelected: null,
+        isFreeEntry: false,
+        freeEntryLatitude: "",
+        freeEntryLongitude: ""
+    };
+  }
 
   componentDidMount() {
+    this.buildDropdown()
+  }
+
+  buildDropdown = () => {
     var list = []
     for(var i = 0; i < this.props.destinations.length; i++) {
       list.push({
@@ -32,30 +35,57 @@ class DiveSiteAddView extends Component {
         value: this.props.destinations[i]
       })
     }
-    console.log(list)
     this.setState({ countryList: list });
   }
 
-  handleChange = (countrySelected) => {
-    console.log(countrySelected)
+  handleNextStep = () => {
+    if(this.state.step === 3) {
+      this.createDiveSite()
+    } else {
+      if(this.state.step === 1) {
+        this.lookupCountryName()
+      }
+      this.setState(prevState => ({
+        step: prevState.step + 1
+      }))
+    }
+  }
+
+  handleCountrySelect = (countrySelected) => {
     this.setState(
-      { countrySelected },
-      () => console.log(`Option selected:`, this.state.countrySelected)
+      { countrySelected }
     );
   };
+
+  toggleFreeEntry = () => {
+    this.setState(prevState => ({
+      isFreeEntry: !prevState.isFreeEntry
+    }))
+  }
+
+  onChangeTextLatitude = input => {
+    this.setState({ freeEntryLatitude: input });
+  }
+
+  onChangeTextLongitude = input => {
+    this.setState({ freeEntryLongitude: input });
+  }
 
   onChangeTextName = input => {
     this.setState({ name: input });
   };
 
-  onSelectCountry = input => {
-    this.setState({ country: input });
-  }
+  lookupCountryName = () => {
+    const latitude = this.state.isFreeEntry ? this.state.freeEntryLatitude : this.props.mapCenter[0]
+    const longitude = this.state.isFreeEntry ? this.state.freeEntryLongitude : this.props.mapCenter[1]
 
-  onSubmitCoordinates = () => {
-    var path = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + this.props.mapCenter[0] + ',' + this.props.mapCenter[1] + '&key=AIzaSyDK1d0EuOMSupb27KqzQJCkTqjSDjXtf-E' + '&result_type=country'
+    var path = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=AIzaSyDK1d0EuOMSupb27KqzQJCkTqjSDjXtf-E' + '&result_type=country'
 
     axios.get(path).then(function (response) {
+      if (!response.data.results[0]) {
+        return
+      }
+
       const code = response.data.results[0].address_components[0].short_name
       for(var i = 0; i < this.state.countryList.length; i++) {
         if (this.state.countryList[i].value.code == code) {
@@ -66,7 +96,12 @@ class DiveSiteAddView extends Component {
     }.bind(this));
   }
 
-  onCreateDiveSite = (name) => {
+  createDiveSite = () => {
+    const latitude = this.state.isFreeEntry ? Number(this.state.freeEntryLatitude) : this.props.mapCenter[0]
+    const longitude = this.state.isFreeEntry ? Number(this.state.freeEntryLongitude) : this.props.mapCenter[1]
+    const name = this.state.countrySelected.value.name
+    const id = this.state.countrySelected.value._id
+
     axios({
       method: 'post',
       url: BaseURL() + '/api/dive-sites',
@@ -75,12 +110,12 @@ class DiveSiteAddView extends Component {
         'Authorization': 'JWT ' + this.props.user.token
       },
       data: {
-        name: name,
-        latitude: this.props.mapCenter[0],
-        longitude: this.props.mapCenter[1],
+        name: this.state.name,
+        latitude: latitude,
+        longitude: longitude,
         destination: {
-          id: 'new-zealand',
-          name: 'New Zealand'
+          id: id,
+          name: name
         }
       }
 
@@ -96,136 +131,91 @@ class DiveSiteAddView extends Component {
   render() {
 
     return (
-      <View style={{width: 320}}>
-        <LocationPicker mapCenter={this.props.mapCenter} />
-        <NamePicker />
-        <View>
-          <Text>{this.state.country}</Text>
-          <Select
-            value={this.state.countrySelected}
-            onChange={this.handleChange}
-            options={this.state.countryList}
-          />
-        </View>
-
-        <View style={{flex: 1}} />
-          
-        <View>
-          <View style={{ flexDirection: 'row', margin: 5, marginBottom: 10}}>
-            <TouchableOpacity style={{backgroundColor: '#A00000', margin: 10, marginBottom: 5, flex: 1, borderRadius: 5}} onPress={this.onSubmitCoordinates}>
-              <Text style={{fontSize: 16, fontWeight: 'bold', textAlign: 'center', padding: 5, margin: 10, color: 'white'}}>Confirm</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{backgroundColor: '#A00000', margin: 10, marginBottom: 5, flex: 1, borderRadius: 5}} onPress={() => this.props.history.goBack()} >
-              <Text style={{fontSize: 16, fontWeight: 'bold', textAlign: 'center', padding: 5, margin: 10, color: 'white'}}>Cancel</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+      <View style={{width: 320, marginTop: 20, flexDirection: 'column-reverse', justifyContent: 'flex-end'}}>
+        <NextButton onPress={this.handleNextStep} step={this.state.step} />
+        {this.state.step > 2 ? <NamePicker onChangeName={this.onChangeTextName} name={this.state.name} /> : <View></View>}
+        {this.state.step > 1 ? <CountryPicker selected={this.state.countrySelected} onChange={this.handleCountrySelect} list={this.state.countryList} /> : <View></View>}
+        <CoordinatesPicker mapCenter={this.props.mapCenter} isFreeEntry={this.state.isFreeEntry} toggleFreeEntry={this.toggleFreeEntry} freeEntryLatitude={this.state.freeEntryLatitude} freeEntryLongitude={this.state.freeEntryLongtiude} onChangeTextLatitude={this.onChangeTextLatitude} onChangeTextLongitude={this.onChangeTextLongitude} />
       </View>
     )
   }
 }
 
-class LocationPicker extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        isFreeEntry: false,
-        freeEntryLatitude: "",
-        freeEntryLongtiude: ""
-    };
-  }
-
-  onChangeTextName = input => {
-    this.setState({ name: input });
-  };
-
-  onChangeTextLatitude = input => {
-    this.setState({ freeEntryLatitude: input });
-  }
-
-  onChangeTextLongitude = input => {
-    this.setState({ freeEntryLongtiude: input });
-  }
-
-  render() {
-
-    return (
-      <View>
-        <View style={{flexDirection: 'row'}}>
-          <View style={{justifyContent: 'center', alignItems: 'center'}}>
-            <View style={{width: 30, height: 30, borderColor: '#A00000', borderWidth: 2, borderRadius: 15}}>
-              <Text>1</Text>
-            </View>
-          </View>
-          
-          {this.state.isFreeEntry ? 
-          <View>
-            <TextInput
-              style={{backgroundColor: '#CCCCCC', padding: 7, fontSize: 20, margin: 20, marginTop: 5, textAlign: 'center' }}
-              onChangeText={text => this.onChangeTextLatitude(text)}
-              value={this.state.freeEntryLatitude}
-            />
-            <TextInput
-              style={{backgroundColor: '#CCCCCC', padding: 7, fontSize: 20, margin: 20, marginTop: 5, textAlign: 'center' }}
-              onChangeText={text => this.onChangeTextLongitude(text)}
-              value={this.state.freeEntryLongitude}
-            />
-          </View> 
-          : 
-          <View style={{flex: 1, justifyContent: 'center'}}>
-            <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>Location</Text>
-            <Text style={{fontSize: 20, paddingBottom: 16, margin: 5, textAlign: 'center'}}>{Number((this.props.mapCenter[0]).toFixed(5))}, {Number((this.props.mapCenter[1]).toFixed(5))} </Text>
-          </View>
-          }
-        </View>
-
-
-        <TouchableOpacity onPress={() => this.setState(prevState => ({isFreeEntry: !prevState.isFreeEntry }))} style={{alignItems: 'center', marginBottom: 30}}>
-          <Text>{this.state.isFreeEntry ? "Find on Map" : "Free Entry"}</Text>
-        </TouchableOpacity>
-      </View>
-    );
-    
-  }
+function NextButton({ onPress, step }) {
+  return (
+    <View style={{alignItems: 'flex-end', marginTop: 20}}>
+      <TouchableOpacity onPress={onPress} style={{backgroundColor: 'red'}}>
+        <Text style={{color: 'white', fontWeight: '600', fontSize: 18, paddingHorizontal: 20, paddingVertical: 10}}>{step > 2 ? "Save" : "Next"}</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
-class NamePicker extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          name: ""
-      };
-    }
+function CountryPicker({ selected, onChange, list}) {
+  return (
+    <View style={{borderTopColor: '#CCCCCC', borderTopWidth: 1, paddingTop: 20}}>
+      <View style={{marginHorizontal: 20}}>
+        <Text style={{fontSize: 20}}>Country</Text>
+        {!selected ? <View></View> : <Text>The dive site appears to be in {selected.label}, is that correct?</Text>}
+      </View>
+      <View style={{margin: 20}}>
+        <Select
+          value={selected}
+          onChange={onChange}
+          options={list}
+        />
+      </View>
+    </View>
+  )
+}
 
-  componentDidMount(){
-    this.nameInput.focus();
-  }
+function CoordinatesPicker({ mapCenter, isFreeEntry, toggleFreeEntry, freeEntryLatitude, freeEntryLongitude, onChangeTextLatitude, onChangeTextLongitude}) {
+  return (
+    <View>
+      <View style={{marginHorizontal: 20}}>
+        <Text style={{fontSize: 20}}>Location</Text>
+        <TouchableOpacity onPress={toggleFreeEntry}>
+          <Text><span style={{color: isFreeEntry ? 'red' : 'black'}} >Find the spot with the map</span>, or <span style={{color: isFreeEntry ? 'black' : 'red'}} >enter the coordinates manually</span>.</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={{flexDirection: 'row', margin: 10}}>
+        <View style={{backgroundColor: '#CCCCCC', flex: 1, height: 50, margin: 10}}>
+          {isFreeEntry ? 
+          <TextInput
+            style={{backgroundColor: '#CCCCCC', flex: 1, fontSize: 20, textAlign: 'center' }}
+            onChangeText={text => onChangeTextLatitude(text)}
+            value={freeEntryLatitude}
+          /> : <Text>{Number((mapCenter[0]).toFixed(5))}</Text>}
+        </View>
+        <View style={{backgroundColor: '#CCCCCC', flex: 1, height: 50, margin: 10}}>
+          {isFreeEntry ? 
+            <TextInput
+              style={{backgroundColor: '#CCCCCC', flex: 1, fontSize: 20, textAlign: 'center' }}
+              onChangeText={text => onChangeTextLongitude(text)}
+              value={freeEntryLongitude}
+            /> : <Text>{Number((mapCenter[1]).toFixed(5))}</Text>}
+        </View>
+      </View>
+    </View>
+  );
+}
 
-  onChangeName = input => {
-    this.setState({ name: input });
-  };
-
-  render() {
-    return (
+function NamePicker({ onChangeName, name }) {
+  return (
+    <View style={{borderTopColor: '#CCCCCC', borderTopWidth: 1, paddingTop: 20}}>
       <View style={{flexDirection: 'row'}}>
-        <View style={{justifyContent: 'center', alignItems: 'center'}}>
-            <View style={{width: 30, height: 30, borderColor: '#A00000', borderWidth: 2, borderRadius: 15}}>
-                <Text>2</Text>
-            </View>
-          </View>
         <View style={{flex: 1, justifyContent: 'center'}}>
           <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>Name</Text>
           <TextInput
-            ref={(input) => { this.nameInput = input; }}
             style={{backgroundColor: '#CCCCCC', padding: 7, fontSize: 20, margin: 20, marginTop: 5, textAlign: 'center' }}
-            onChangeText={text => this.onChangeName(text)}
-            value={this.state.name}
+            onChangeText={text => onChangeName(text)}
+            value={name}
             />
         </View>
       </View>
-
-    )
-  }
+    </View>
+  )
 }
 
 const mapStateToProps = state => {
