51dc5eadb8e052a1b62980d22e1e592671c9a853
Rob Broadwell <rob@inspiringapps.com>
2020-06-27 10:52:41 -0400

    split out detail sidebar cards

--- src/components/dive-sites/DiveSiteDetail.js
+++ src/components/dive-sites/DiveSiteDetail.js
@@ -87,6 +87,131 @@ function DiveSiteDetailBody({ diveSite, reload }) {
   )
 }
 
+function VisibilityCard({ diveSite }) {
+  return (
+    <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
+      <ReactPlaceholder type='rect' style={{height: 80}} ready={diveSite} showLoadingAnimation={true}>
+        <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
+          <Text style={{fontSize: 18, fontWeight: '600'}}>Visibility</Text>
+          <View style={{flex: 1}} />
+          <Text style={{color: '#A00000'}}>Log Dive</Text>
+          <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
+        </View>
+        <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
+          <Image style={{width: 50, height: 30, marginTop: 0, marginLeft: 10, marginRight: 10}} source={require('../../assets/dial.svg')} />
+          <Text style={{fontWeight: '600'}}>100+ meters</Text>
+          <View style={{flex: 1}} />
+          <Text>See history</Text>
+        </View>
+      </ReactPlaceholder>
+    </View>
+  )
+}
+
+function DepthCard({ diveSite }){
+  return (
+    <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
+      <ReactPlaceholder type='rect' style={{height: 80}} ready={diveSite} showLoadingAnimation={true}>
+        <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
+          <Text style={{fontSize: 18, fontWeight: '600'}}>Depth</Text>
+          <View style={{flex: 1}} />
+          <Text style={{color: '#A00000'}}>Log Dive</Text>
+          <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
+        </View>
+        <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
+          <Image style={{width: 50, height: 30, marginTop: 0, marginLeft: 10, marginRight: 10}} source={require('../../assets/dial.svg')} />
+          <View style={{flexDirection: 'column'}}>
+            <Text style={{fontWeight: '600'}}>Average: 44 ft</Text>
+            <Text style={{fontWeight: '600'}}>Max: 78 ft</Text>
+          </View>
+          <View style={{flex: 1}} />
+          <Text>See history</Text>
+        </View>
+      </ReactPlaceholder>
+    </View>
+  )
+}
+
+function WaterTemperatureCard({ diveSite }) {
+  return (
+    <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
+      <ReactPlaceholder type='rect' style={{height: 80}} ready={diveSite} showLoadingAnimation={true}>
+        <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
+          <Text style={{fontSize: 18, fontWeight: '600'}}>Water Temperature</Text>
+          <View style={{flex: 1}} />
+          <Text style={{color: '#A00000'}}>Log Dive</Text>
+          <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
+        </View>
+        <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
+          <Image style={{width: 50, height: 30, marginTop: 0, marginLeft: 10, marginRight: 10}} source={require('../../assets/dial.svg')} />
+          <Text style={{fontWeight: '600'}}>80°F</Text>
+          <View style={{flex: 1}} />
+          <Text>See history</Text>
+        </View>
+      </ReactPlaceholder>
+    </View>
+  )
+}
+
+function CurrentsCard({ diveSite }) {
+  return (
+    <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
+      <ReactPlaceholder type='rect' style={{height: 80}} ready={diveSite} showLoadingAnimation={true}>
+        <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
+          <Text style={{fontSize: 18, fontWeight: '600'}}>Currents</Text>
+          <View style={{flex: 1}} />
+          <Text style={{color: '#A00000'}}>Log Dive</Text>
+          <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
+        </View>
+        <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
+          <Image style={{width: 50, height: 30, marginTop: 0, marginLeft: 10, marginRight: 10}} source={require('../../assets/dial.svg')} />
+          <Text style={{fontWeight: '600'}}>None</Text>
+          <View style={{flex: 1}} />
+          <Text>See history</Text>
+        </View>
+      </ReactPlaceholder>
+    </View>
+  )
+}
+
+function AccessCard({ diveSite }) {
+  return (
+    <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
+      <ReactPlaceholder type='rect' style={{height: 60}} ready={diveSite} showLoadingAnimation={true}>
+        <View style={{flexDirection: 'row', margin: 10, marginBottom: 3, alignItems: 'center'}}>
+          <Text style={{fontSize: 18, fontWeight: '600'}}>Access</Text>
+          <View style={{flex: 1}} />
+          <Text style={{color: '#A00000'}}>Edit</Text>
+          <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
+        </View>
+        <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
+          <Text style={{fontWeight: '600'}}>Boat, Liveaboard</Text>
+          <View style={{flex: 1}} />
+        </View>
+      </ReactPlaceholder>
+    </View>
+  )
+}
+
+function DiveTypeCard({ diveSite }) {
+  return (
+    <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
+      <ReactPlaceholder type='rect' style={{height: 60}} ready={diveSite} showLoadingAnimation={true}>
+        <View style={{flexDirection: 'row', margin: 10, marginBottom: 3, alignItems: 'center'}}>
+          <Text style={{fontSize: 18, fontWeight: '600'}}>Dive Type</Text>
+          <View style={{flex: 1}} />
+          <Text style={{color: '#A00000'}}>Edit</Text>
+          <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
+        </View>
+        <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
+          <Text style={{fontWeight: '600'}}>Shipwreck</Text>
+          <View style={{flex: 1}} />
+        </View>
+      </ReactPlaceholder>
+    </View>
+  )
+}
+
 function DiveSiteDetailSidebar({ diveSite }) {
   return (
     <StickyContainer>
@@ -94,89 +219,13 @@ function DiveSiteDetailSidebar({ diveSite }) {
         {/* <View style={{height: 200, borderColor: '#DDDDDD', borderWidth: 1, margin: 10}}>
           <Image style={{flex: 1}} source={require('../../assets/weather_placeholder2.png')} />
         </View> */}
-        <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
-          <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
-            <Text style={{fontSize: 18, fontWeight: '600'}}>Visibility</Text>
-            <View style={{flex: 1}} />
-            <Text style={{color: '#A00000'}}>Log Dive</Text>
-            <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
-          </View>
-          <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
-            <Image style={{width: 50, height: 30, marginTop: 0, marginLeft: 10, marginRight: 10}} source={require('../../assets/dial.svg')} />
-            <Text style={{fontWeight: '600'}}>100+ meters</Text>
-            <View style={{flex: 1}} />
-            <Text>See history</Text>
-          </View>
-        </View>
-        <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
-          <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
-            <Text style={{fontSize: 18, fontWeight: '600'}}>Depth</Text>
-            <View style={{flex: 1}} />
-            <Text style={{color: '#A00000'}}>Log Dive</Text>
-            <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
-          </View>
-          <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
-            <Image style={{width: 50, height: 30, marginTop: 0, marginLeft: 10, marginRight: 10}} source={require('../../assets/dial.svg')} />
-            <View style={{flexDirection: 'column'}}>
-              <Text style={{fontWeight: '600'}}>Average: 44 ft</Text>
-              <Text style={{fontWeight: '600'}}>Max: 78 ft</Text>
-            </View>
-            <View style={{flex: 1}} />
-            <Text>See history</Text>
-          </View>
-        </View>
-        <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
-          <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
-            <Text style={{fontSize: 18, fontWeight: '600'}}>Water Temperature</Text>
-            <View style={{flex: 1}} />
-            <Text style={{color: '#A00000'}}>Log Dive</Text>
-            <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
-          </View>
-          <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
-            <Image style={{width: 50, height: 30, marginTop: 0, marginLeft: 10, marginRight: 10}} source={require('../../assets/dial.svg')} />
-            <Text style={{fontWeight: '600'}}>80°F</Text>
-            <View style={{flex: 1}} />
-            <Text>See history</Text>
-          </View>
-        </View>
-        <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
-          <View style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
-            <Text style={{fontSize: 18, fontWeight: '600'}}>Currents</Text>
-            <View style={{flex: 1}} />
-            <Text style={{color: '#A00000'}}>Log Dive</Text>
-            <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
-          </View>
-          <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
-            <Image style={{width: 50, height: 30, marginTop: 0, marginLeft: 10, marginRight: 10}} source={require('../../assets/dial.svg')} />
-            <Text style={{fontWeight: '600'}}>None</Text>
-            <View style={{flex: 1}} />
-            <Text>See history</Text>
-          </View>
-        </View>
-        <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
-          <View style={{flexDirection: 'row', margin: 10, marginBottom: 3, alignItems: 'center'}}>
-            <Text style={{fontSize: 18, fontWeight: '600'}}>Access</Text>
-            <View style={{flex: 1}} />
-            <Text style={{color: '#A00000'}}>Edit</Text>
-            <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
-          </View>
-          <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
-            <Text style={{fontWeight: '600'}}>Boat, Liveaboard</Text>
-            <View style={{flex: 1}} />
-          </View>
-        </View>
-        <View style={{margin: 10, marginBottom: 0, borderColor: '#DDDDDD', borderWidth: 1}}>
-          <View style={{flexDirection: 'row', margin: 10, marginBottom: 3, alignItems: 'center'}}>
-            <Text style={{fontSize: 18, fontWeight: '600'}}>Dive Type</Text>
-            <View style={{flex: 1}} />
-            <Text style={{color: '#A00000'}}>Edit</Text>
-            <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/edit.svg')} />
-          </View>
-          <View style={{flexDirection: 'row', marginHorizontal: 10, marginBottom: 10}}>
-            <Text style={{fontWeight: '600'}}>Shipwreck</Text>
-            <View style={{flex: 1}} />
-          </View>
-        </View>
+        
+        <VisibilityCard diveSite={diveSite} />
+        <DepthCard diveSite={diveSite} />
+        <WaterTemperatureCard diveSite={diveSite} />
+        <CurrentsCard diveSite={diveSite} />
+        <AccessCard diveSite={diveSite} />
+        <DiveTypeCard diveSite={diveSite} />
 
         {/* <DiveSiteDetailMarketing /> */}
       </View>
