0b6088f4399dcb83cdf4d8811941566173c935f5
Rob Broadwell <rob@inspiringapps.com>
2020-06-29 15:53:44 -0400

    Revert "remove comments"
    This reverts commit a8180cd185c945bba0aa46465b861a3102204773.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -89,6 +89,11 @@ export function setDiveSite(site) {
 
 export function fetchDestinations() {
   return function(dispatch, getState) {
+
+    // if (getState().destinations.length !== 0) {
+    //   return
+    // }
+
     return fetch(BaseURL() + '/api/destinations')
       .then((response) => response.json())
       .then((json) => {
@@ -104,6 +109,14 @@ export function fetchDestinations() {
 
 export function fetchTopDestinations() {
   return function(dispatch, getState) {
+
+    console.log(process.env.NODE_ENV === "development")
+
+    // if (getState().topDestinations.length !== 0) {
+    //   return
+    // }
+    
+
     return fetch(BaseURL() + '/api/destinations/top')
       .then((response) => response.json())
       .then((json) => {
