0c26020cd20a354b2a3f32e1f8ec9174df765c6d
Rob Broadwell <rob@inspiringapps.com>
2020-06-30 11:17:34 -0400

    reviews full width bug

--- src/components/dive-sites/DiveSiteDetailSidebar.js
+++ src/components/dive-sites/DiveSiteDetailSidebar.js
@@ -7,7 +7,7 @@ import DiveSiteReviewsList from './DiveSiteReviewsList';
 function DiveSiteDetailSidebar({ diveSite, openAddReview }) {
 
   return (
-    <View style={{width: 450, paddingLeft: 20, paddingTop: 10, paddingRight: 20, marginRight: -10, marginBottom: -10, backgroundColor: '#EFEFEF', flexDirection: 'column', alignItems: 'center'}}>
+    <View style={{width: 450, paddingLeft: 20, paddingTop: 10, paddingRight: 20, marginRight: -10, marginBottom: -10, backgroundColor: '#EFEFEF', flexDirection: 'column'}}>
       {/* <View style={{height: 200, borderColor: '#DDDDDD', borderWidth: 1, margin: 10}}>
         <Image style={{flex: 1}} source={require('../../assets/weather_placeholder2.png')} />
       </View> */}
