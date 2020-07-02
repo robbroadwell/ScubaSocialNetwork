4d56e05be56e1a2e6278938b29d757dfd066b876
Rob Broadwell <rob@inspiringapps.com>
2020-07-02 19:02:39 -0400

    bug with search / add dive site coexisting

--- src/components/dive-sites/DiveSiteAdd.js
+++ src/components/dive-sites/DiveSiteAdd.js
@@ -6,7 +6,7 @@ import DiveSiteAddView from './DiveSiteAddView';
 class DiveSiteAdd extends Component {
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'row', height: '75vh'}}>
+      <View style={{flexDirection: 'row', height: '80vh'}}>
         <Map style={this.props.style} diveSites={this.props.diveSites} addDiveSite={true} hideList={true} />
         <DiveSiteAddView history={this.props.history} />
       </View>
