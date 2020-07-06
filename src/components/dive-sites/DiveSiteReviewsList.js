0b5d5d636e217fbda109e332f48808ce1fdbb824
Rob Broadwell <rob@inspiringapps.com>
2020-07-06 10:52:10 -0400

    Revert "likes on reviews"
    This reverts commit c2246fb36f1f28fbe7797dcb48b7719ea7aeb5d8.

--- src/components/dive-sites/DiveSiteReviewsList.js
+++ src/components/dive-sites/DiveSiteReviewsList.js
@@ -19,7 +19,7 @@ class DiveSiteReviewsList extends Component {
         const review = diveSite.reviews[i]
     
         views.push(
-          <View key={i} style={{borderColor: '#DDDDDD', borderWidth: 1, marginBottom: 10, padding: 15, backgroundColor: '#FEFEFE', shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.15, shadowRadius: 2, shadowColor: '#000'}}>
+          <View key={i} style={{borderColor: '#CCCCCC', borderWidth: 1, marginBottom: 10, padding: 15, backgroundColor: '#FEFEFE', shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.1, shadowRadius: 5, shadowColor: '#000'}}>
 
             <View style={{flex: 1, alignItems: 'center'}}>
               <Text style={{fontWeight: '600', fontSize: 18, marginVertical: 5}}>{review.title}</Text>
@@ -38,10 +38,6 @@ class DiveSiteReviewsList extends Component {
             
             <Text style={{fontSize: 14, marginVertical: 20, maxWidth: 500}}>{review.comment}</Text>
             <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <View style={{paddingHorizontal: 20, flexDirection: 'row'}}>
-                <Image style={{height: 20, width: 20, tintColor: '#3333333'}} source={require('../../assets/heart_outline.svg')} />
-                <Text style={{marginLeft: 5}}>14</Text>
-              </View>
               <View style={{flex: 1}} />
               <UserBadge user={review.user} timestamp={review.timestamp} />
             </View>
@@ -52,10 +48,6 @@ class DiveSiteReviewsList extends Component {
   
     return (
       <View>        
-        <View style={{flexDirection: 'row', justifyContent: 'flex-end', marginBottom: 5}}>
-          <Text>Top Reviews</Text>
-          <Image style={{width: 15, height: 15, marginLeft: 5}} source={require('../../assets/drop_down.svg')} />
-        </View>
         <View style={{flex: 1}}>
           {views}
         </View>
