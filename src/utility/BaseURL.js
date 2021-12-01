827edaf602ed745edcb91f4164139116673537ea
Rob Broadwell <rob@inspiringapps.com>
2021-12-01 22:22:24 +0200

    remove slash

--- src/utility/BaseURL.js
+++ src/utility/BaseURL.js
@@ -2,6 +2,6 @@ export default function BaseURL() {
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:8080"
   } else {
-    return "https://divingscore.herokuapp.com/"
+    return "https://divingscore.herokuapp.com"
   }
 }
